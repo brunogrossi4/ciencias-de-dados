0d3caf4b829e3d24973860fc5ce8c73c03581ca7
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2024-10-17 10:39:46 -0300

    vr

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,15 @@
-const url = 'https://raw.githubsercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glabaison
 
-async function visualizarInformacoesGlobais(){
+async function visualizarInformacoesGlobais() {
+  const res await fetch(url)
+  const dados = res.json()
+  const paragrafo document.createElement('p') paragrafo.classList.add('graficos-container texto')
+  paragrafo.innerHTML = Você sabia que o mundo tem
+  $(dados.total_pessoas_mundo) de pessoas e que aproximadamente
+  $(dados.total_pessoas_conectadas) estão conectadas em alguma rede social e passan en sétia $(dados.tempo medio) horas conectadas.
+  const containder = document.getElementById('graficos-container")
+  containder.appendChild(paragrafo)
+
 }
 
 visualizarInformacoesGlobais();
