03917ff2674937e8650e4b71625d5f99bae8784c
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2024-11-21 10:40:00 -0300

    u

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,7 +4,7 @@ async function visualizarInformacoesGlobais() {
   const res = await fetch(url)
   const dados = res.json()
   const praticamRegularmente = (dados.total_pessoas_que_praticam_esportes_regularmente) / 1e9
-  const pessoaNoMundo = (dados.total_pessoas_no mundo) / 1e9
+  const pessoaNoMundo = (dados.total_pessoas_no_mundo) / 1e9
   const horas = parseInt(dados.tempo_medio)
   const minutos = Math.round(( dados.tempo-medio -horas) *100)
   const porentagemConectada = ((praticamRegularmente  /  pessoaNoMundo) * 100).toFixed(2)
