1a2a0698fbbcaa50587a3fb77bcc8823f9dc14d8
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2024-11-21 10:22:33 -0300

    g

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,15 +1,20 @@
-const url https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glabaison
+const url = 'https://raw.githubusercontent.com/Raymarques20/ciencia-de-dados/refs/heads/main/base-de-dados/esportes-dados-globais%20(1).json'
 
 async function visualizarInformacoesGlobais() {
-  const res await fetch(url)
+  const res = await fetch(url)
   const dados = res.json()
-  const paragrafo document.createElement('p') paragrafo.classList.add('graficos-container texto')
-  paragrafo.innerHTML = Você sabia que o mundo tem
-  $(dados.total_pessoas_mundo) de pessoas e que aproximadamente
-  $(dados.total_pessoas_conectadas) estão conectadas em alguma rede social e passan en sétia $(dados.tempo medio) horas conectadas.
-  const containder = document.getElementById('graficos-container")
-  containder.appendChild(paragrafo)
-
+  const praticamRegularmente = (dados.total_pessoas_que_praticam_esportes_regularmente) / 1e9
+  const pessoaNoMundo = (dados.total_pessoas_no mundo) / 1e9
+  const horas = parseInt(dados.tempo_medio)
+  const minutos = Math.round(( dados.tempo-medio -horas) *100)
+  const porentagemConectada = ((praticamRegularmente  /  pessoaNoMundo) * 100).toFixed(2)
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasMundo} bilhões</span> de pessoas e que aproximadamente <span>${praticamRegularmente} bilhões </span> estão conectadas em alguma rede social e passam em média <span>${horas} </span> horas e <span>${minutos}</span> minutos-conectadas.`
+  <br> Isso significa que aproximadamente ${porcentagemConetada} de pessoas que praticam regularmente algum esporte SpeechRecognitionAlternative.
+  
+  const container = document.getElementById('graficos-container')
+  container.appendChild(paragrafo)
 }
 
 visualizarInformacoesGlobais();
